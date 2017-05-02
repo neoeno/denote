139d20d484d688c070295a928fe8e7028b3932a2
Kay Lovelace <kay@makersacademy.com>
2017-05-02 15:30:43 +0100

    Run prettier

--- denote.js
+++ denote.js
@@ -7,12 +7,12 @@ import git from 'simple-git'
 import userHome from 'user-home'
 
 const root = `${userHome}/notes`
-const weekDirectory = moment().startOf('isoWeek').format("[WC]-YYYY-MM-DD")
-const dayFilename = moment().format("YYYMMDDHHmmss-dddd-hh-mma")
+const weekDirectory = moment().startOf('isoWeek').format('[WC]-YYYY-MM-DD')
+const dayFilename = moment().format('YYYMMDDHHmmss-dddd-hh-mma')
 const directoryPath = `${root}/${weekDirectory}`
 const filename = `${directoryPath}/${dayFilename}.md`
 
-const headingDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+const headingDate = moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
 const heading = `# Note for ${headingDate}`
 
 const writeHeaderAnd = fn => {
@@ -22,28 +22,29 @@ const writeHeaderAnd = fn => {
   })
 }
 
-const openEditorAnd = (fn) => {
+const openEditorAnd = fn => {
   editor(filename, fn)
 }
 
-const pullAsynchronouslyAnd = (fn) => {
-  git(root)
-    .pull('origin', 'master')
+const pullAsynchronouslyAnd = fn => {
+  git(root).pull('origin', 'master')
   fn()
 }
 
-const makeDirectoryAnd = (fn) => {
+const makeDirectoryAnd = fn => {
   fs.mkdir(directoryPath, err => {
     if (err && err.code !== 'EEXIST') throw err
     fn()
   })
 }
 
-const deleteBlankNoteOr = (fn) => {
+const deleteBlankNoteOr = fn => {
   fs.readFile(filename, 'utf8', (err, contents) => {
-    if (contents.split("\n").join(" ").match(new RegExp(`^\\s*${heading}\\s*$`))) {
+    if (
+      contents.split('\n').join(' ').match(new RegExp(`^\\s*${heading}\\s*$`))
+    ) {
       trash(filename).then(() => {
-        console.log("Nothing written! Trashed the file")
+        console.log('Nothing written! Trashed the file')
       })
     } else {
       fn()
@@ -52,16 +53,18 @@ const deleteBlankNoteOr = (fn) => {
 }
 
 const gitCommitAndPush = () => {
-  console.log("Pushing to git remote...")
+  console.log('Pushing to git remote...')
   git(root)
     .add('./*')
     .commit(`Add log for ${headingDate}`)
     .push('origin', 'master')
-    .then(() => console.log("Pushed!"))
+    .then(() => console.log('Pushed!'))
 }
 
 const chain = (fn, ...fns) => () => {
-  if (fn == undefined) { return }
+  if (fn == undefined) {
+    return
+  }
   return fn(chain(...fns))
 }
 
